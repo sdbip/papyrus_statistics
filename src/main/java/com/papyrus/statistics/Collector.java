f6135ff01a2e130b039e61f799cb22cca5d523a8
Johan Segerfeldt <johan.segerfeldt@me.com>
2017-10-02 00:57:17 +0200

    Add first test

---
+++ src/main/java/com/papyrus/statistics/Collector.java
@@ -0,0 +1,14 @@
+package com.papyrus.statistics;
+
+import java.util.Collections;
+import java.util.List;
+
+class Collector {
+    void add(final String step, final String measure, double value) {
+
+    }
+
+    List<Double> get(final String step, final String measure) {
+        return Collections.singletonList(9.0);
+    }
+}
---
+++ src/test/java/com/papyrus/statistics/CollectionTest.java
@@ -0,0 +1,18 @@
+package com.papyrus.statistics;
+
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+public class CollectionTest {
+    @Test
+    public void collectsSingleValue() {
+        final Collector collector = new Collector();
+
+        collector.add("Picking", "Duration", 9.0);
+
+        assertEquals(Collections.singletonList(9.0), collector.get("Picking", "Duration"));
+    }
+}
