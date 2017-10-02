57e3ba3d25e3133d53bc530035f8d8df5503dc45
Johan Segerfeldt <johan.segerfeldt@me.com>
2017-10-02 10:56:28 +0200

    Extract method for duplicated code

--- src/main/java/com/papyrus/statistics/Calculator.java
+++ src/main/java/com/papyrus/statistics/Calculator.java
@@ -6,19 +6,24 @@ final class Calculator {
     CalculatedData calculate(final CollectedData collectedData) {
         final CalculatedData calculatedData = new CalculatedData();
         for (final Measurement measurement : collectedData.measurements.keySet()) {
-            final List<Double> values = collectedData.measurements.get(measurement);
-            final double average = average(values);
-            calculatedData.entries.put(measurement.step, new CalculatedEntry(average, 0));
+            final CalculatedEntry entry = calculateEntry(collectedData, measurement);
+            calculatedData.entries.put(measurement.step, entry);
         }
 
         for (final Measurement measurement : collectedData.errors.keySet()) {
-            final List<Double> values = collectedData.measurements.get(measurement);
-            final double average = values == null ? 0 : average(values);
-            final int errors = collectedData.errors.get(measurement);
-            calculatedData.entries.put(measurement.step, new CalculatedEntry(average, errors));
+            final CalculatedEntry entry = calculateEntry(collectedData, measurement);
+            calculatedData.entries.put(measurement.step, entry);
         }
 
         return calculatedData;
+    }
+
+    private CalculatedEntry calculateEntry(final CollectedData collectedData, final Measurement measurement) {
+        final List<Double> maybeValues = collectedData.measurements.get(measurement);
+        final double average = maybeValues == null ? 0 : average(maybeValues);
+        final Integer maybeErrors = collectedData.errors.get(measurement);
+        final int errors = maybeErrors == null ? 0 : maybeErrors;
+        return new CalculatedEntry(average, errors);
     }
 
     private double average(List<Double> values) {
