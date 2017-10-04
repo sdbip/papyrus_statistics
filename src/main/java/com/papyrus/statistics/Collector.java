5f91b5f4f01396676317240c2f02a9a6d8a4ec2d
Johan Segerfeldt <johan.segerfeldt@me.com>
2017-10-04 05:36:32 +0200

    Inline methods and remove field

--- src/main/java/com/papyrus/statistics/Collector.java
+++ src/main/java/com/papyrus/statistics/Collector.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 
 class Collector {
-    private final CollectedData collectedData = new CollectedData();
     private final Source source;
 
     Collector(final Source source) {
@@ -14,24 +13,35 @@ class Collector {
     }
 
     CalculatedData collect() {
+        final CollectedData collectedData = new CollectedData();
         for (final CollectedEntry entry : source.entries()) {
-            if (entry.isError)
-                reportError(entry.measurement);
-            else
-                add(entry.measurement, entry.value);
+            if (entry.isError) {
+                final Integer storedValue = collectedData.errors.get(entry.measurement);
+                final int before = storedValue == null ? 0 : storedValue;
+                collectedData.errors.put(entry.measurement, before + 1);
+            }
+            else {
+                final List<Double> values = collectedData.measurements.computeIfAbsent(entry.measurement, k -> new ArrayList<>());
+                values.add(entry.value);
+            }
         }
-        return calculate(collectedData);
-    }
 
-    CalculatedData calculate(final CollectedData collectedData) {
         final CalculatedData calculatedData = new CalculatedData();
         for (final Measurement measurement : collectedData.measurements.keySet()) {
-            final CalculatedEntry entry = calculateEntry(collectedData, measurement);
+            final List<Double> maybeValues = collectedData.measurements.get(measurement);
+            final double average = maybeValues == null ? 0 : average(maybeValues);
+            final Integer maybeErrors = collectedData.errors.get(measurement);
+            final int errors = maybeErrors == null ? 0 : maybeErrors;
+            final CalculatedEntry entry = new CalculatedEntry(average, errors);
             addEntry(measurement, entry, calculatedData.entries);
         }
 
         for (final Measurement measurement : collectedData.errors.keySet()) {
-            final CalculatedEntry entry = calculateEntry(collectedData, measurement);
+            final List<Double> maybeValues = collectedData.measurements.get(measurement);
+            final double average = maybeValues == null ? 0 : average(maybeValues);
+            final Integer maybeErrors = collectedData.errors.get(measurement);
+            final int errors = maybeErrors == null ? 0 : maybeErrors;
+            final CalculatedEntry entry = new CalculatedEntry(average, errors);
             addEntry(measurement, entry, calculatedData.entries);
 
             final Integer errorsBefore = calculatedData.totalErrorsByMeasure.computeIfAbsent(measurement.measure, k -> 0);
@@ -50,14 +60,6 @@ class Collector {
         entriesByMeasure.put(measurement.measure, entry);
     }
 
-    private CalculatedEntry calculateEntry(final CollectedData collectedData, final Measurement measurement) {
-        final List<Double> maybeValues = collectedData.measurements.get(measurement);
-        final double average = maybeValues == null ? 0 : average(maybeValues);
-        final Integer maybeErrors = collectedData.errors.get(measurement);
-        final int errors = maybeErrors == null ? 0 : maybeErrors;
-        return new CalculatedEntry(average, errors);
-    }
-
     private double average(final List<Double> values) {
         return sum(values) / values.size();
     }
@@ -67,20 +69,5 @@ class Collector {
         for (final double value : values)
             total += value;
         return total;
-    }
-
-    private void add(Measurement measurement, double value) {
-        final List<Double> values = collectedData.measurements.computeIfAbsent(measurement, k -> new ArrayList<>());
-        values.add(value);
-    }
-
-    private void reportError(Measurement measurement) {
-        final int before = errorCount(measurement);
-        collectedData.errors.put(measurement, before + 1);
-    }
-
-    private int errorCount(Measurement measurement) {
-        final Integer storedValue = collectedData.errors.get(measurement);
-        return storedValue == null ? 0 : storedValue;
     }
 }
