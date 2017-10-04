b68ce87466efc7fa6fae17c9498c0f7cff85cf45
Johan Segerfeldt <johan.segerfeldt@me.com>
2017-10-04 08:18:57 +0200

    Extract duplicated code from if branches

--- src/main/java/com/papyrus/statistics/input/Collector.java
+++ src/main/java/com/papyrus/statistics/input/Collector.java
@@ -16,30 +16,19 @@ public class Collector {
         final CollectedData collectedData = new CollectedData();
 
         for (final InputEntry entry : source.entries()) {
+            final Map<Measure, CollectedEntry> entriesForStep = collectedData.entries.computeIfAbsent(entry.step, k -> new HashMap<>());
+            CollectedEntry collectedEntry = entriesForStep.get(entry.measure);
+            if (collectedEntry == null) {
+                collectedEntry = new CollectedEntry(0, 0, 0);
+            }
+            final CollectedEntry changedEntry = entry.isError ?
+                    new CollectedEntry(collectedEntry.total, collectedEntry.count, collectedEntry.errors + 1) :
+                    new CollectedEntry(collectedEntry.total + entry.value, collectedEntry.count + 1, 0);
+            entriesForStep.put(entry.measure, changedEntry);
+
             if (entry.isError) {
                 final Integer errorsBefore = collectedData.totalErrorsByMeasure.get(entry.measure);
                 collectedData.totalErrorsByMeasure.put(entry.measure, errorsBefore == null ? 1 : errorsBefore + 1);
-
-                final Map<Measure, CollectedEntry> entriesForStep = collectedData.entries.computeIfAbsent(entry.step, k -> new HashMap<>());
-                CollectedEntry collectedEntry = entriesForStep.get(entry.measure);
-                if (collectedEntry == null) {
-                    collectedEntry = new CollectedEntry(0, 0, 1);
-                    entriesForStep.put(entry.measure, collectedEntry);
-                } else {
-                    collectedEntry = new CollectedEntry(collectedEntry.total, collectedEntry.count, collectedEntry.errors + 1);
-                    entriesForStep.put(entry.measure, collectedEntry);
-                }
-            }
-            else {
-                final Map<Measure, CollectedEntry> entriesForStep = collectedData.entries.computeIfAbsent(entry.step, k -> new HashMap<>());
-                CollectedEntry collectedEntry = entriesForStep.get(entry.measure);
-                if (collectedEntry == null) {
-                    collectedEntry = new CollectedEntry(entry.value, 1, 0);
-                    entriesForStep.put(entry.measure, collectedEntry);
-                } else {
-                    collectedEntry = new CollectedEntry(collectedEntry.total + entry.value, collectedEntry.count + 1, 0);
-                    entriesForStep.put(entry.measure, collectedEntry);
-                }
             }
         }
 
